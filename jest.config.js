5362135bd4394ce71dfd9dd67fb9549233da922b
Ryan Ling <ryan@outlook.com.au>
2020-06-05 14:48:58 +1000

    Disable colour output in tests

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  coverageDirectory: 'coverage',
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/test.ts',
-    '<rootDir>/lib/',
-    '<rootDir>/template/',
-  ],
   collectCoverageFrom: [
     '**/*.ts',
     '!**/node_modules/**',
     '!<rootDir>/lib/**',
     '!<rootDir>/coverage/**',
     '!<rootDir>/template/**',
+  ],
+  coverageDirectory: 'coverage',
+  preset: 'ts-jest',
+  setupFiles: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/test.ts',
+    '<rootDir>/lib/',
+    '<rootDir>/template/',
   ],
 };
