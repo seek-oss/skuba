9d86b0b0431b08444f2720960da4a260713010a6
Lumi <lumi.shrestha@gmail.com>
2020-06-16 13:55:30 +1000

    Fix tests and merge file logic

--- src/cli/configure/processing/ignoreFile.ts
+++ src/cli/configure/processing/ignoreFile.ts
@@ -1,13 +1,14 @@
 export const mergeWithIgnoreFile = (templateFile: string) => (
   inputFile?: string,
 ) => {
-  const replacedSkubaSection = inputFile?.replace(
+  if (typeof inputFile === 'undefined') {
+    return `${templateFile}\n`;
+  }
+
+  const replacedSkubaSection = inputFile.replace(
     /# managed by skuba[\s\S]*# end managed by skuba/,
     '',
   );
-  if (replacedSkubaSection) {
-    return `${templateFile}\n\n${replacedSkubaSection}`;
-  }
 
-  return `${templateFile}\n`;
+  return `${templateFile}\n\n${replacedSkubaSection}`;
 };
