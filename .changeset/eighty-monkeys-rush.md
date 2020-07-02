c0b8f1c9d1462fe61701e0db1adc92fb1c4ae7be
Lumi Shrestha <lumi.shrestha@gmail.com>
2020-07-02 13:19:12 +1000

    Use connection reuse environment variable in Lambda (#130)

---
+++ .changeset/eighty-monkeys-rush.md
@@ -0,0 +1,5 @@
+---
+'skuba': patch
+---
+
+template/lambda-sqs-worker: Use connection reuse environment variable
--- template/lambda-sqs-worker/src/services/aws.ts
+++ template/lambda-sqs-worker/src/services/aws.ts
@@ -1,13 +1,5 @@
-import { Agent } from 'https';
-
 import { SNS } from 'aws-sdk';
-
-const agent = new Agent({
-  keepAlive: true,
-  rejectUnauthorized: true,
-});
 
 export const sns = new SNS({
   apiVersion: '2010-03-31',
-  httpOptions: { agent },
 });
