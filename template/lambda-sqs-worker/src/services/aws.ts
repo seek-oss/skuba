5ce871a086bed868a8af3668cb65f2ee4b102d57
Lumi <lumi.shrestha@gmail.com>
2020-07-02 13:14:43 +1000

    Remove custom agent

--- template/lambda-sqs-worker/src/services/aws.ts
+++ template/lambda-sqs-worker/src/services/aws.ts
@@ -1,12 +1,5 @@
-import { Agent } from 'https';
-
 import { SNS } from 'aws-sdk';
-
-const agent = new Agent({
-  rejectUnauthorized: true,
-});
 
 export const sns = new SNS({
   apiVersion: '2010-03-31',
-  httpOptions: { agent },
 });
