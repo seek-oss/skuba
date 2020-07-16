e4ab797307bd3eabed48015278708ae456ffc3b2
Ryan Ling <ryan@outlook.com.au>
2020-07-16 18:40:36 +1000

    Copy missed file

--- template/koa-rest-api/src/testing/types.ts
+++ template/koa-rest-api/src/testing/types.ts
@@ -1,18 +1,23 @@
+/* eslint-disable new-cap */
+
 import { Chance } from 'chance';
-import * as yup from 'yup';
+import * as t from 'runtypes';
+import checkFilter from 'runtypes-filter';
 
 import { JobInput } from 'src/types/jobs';
 
+export type IdDescription = t.Static<typeof IdDescription>;
+
+const IdDescription = t.Record({
+  id: t.String,
+  description: t.String,
+});
+
+export const filterIdDescription = checkFilter(IdDescription);
+
 export const chance = new Chance();
 
-export const idDescriptionSchema = yup
-  .object({
-    id: yup.string().required(),
-    description: yup.string().required(),
-  })
-  .required();
-
-export const mockIdDescription = () => ({
+export const mockIdDescription = (): IdDescription => ({
   id: chance.guid({ version: 4 }),
   description: chance.sentence(),
 });
