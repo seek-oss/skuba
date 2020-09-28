18beba0fc35a60218bae72aa93b60d9125f87019
Ryan Ling <ryan@outlook.com.au>
2020-09-28 21:33:13 +1000

    Customise default branch for semantic-release

--- src/cli/release.ts
+++ src/cli/release.ts
@@ -1,5 +1,40 @@
-import { exec } from '../utils/exec';
+import semanticRelease from 'semantic-release';
+
+import { log } from '../utils/logging';
+
+const DEFAULT_BRANCH =
+  process.env.DEFAULT_BRANCH_NAME ||
+  process.env.BUILDKITE_PIPELINE_DEFAULT_BRANCH ||
+  'master';
+
+/**
+ * {@link https://semantic-release.gitbook.io/semantic-release/usage/configuration#branches}
+ */
+const BRANCHES = [
+  '+([0-9])?(.{+([0-9]),x}).x',
+  DEFAULT_BRANCH,
+  'next',
+  'next-major',
+  {
+    name: 'beta',
+    prerelease: true,
+  },
+  {
+    name: 'alpha',
+    prerelease: true,
+  },
+];
 
 export const release = async () => {
-  await exec('semantic-release', '--success', 'false');
+  const result = await semanticRelease({
+    branches: BRANCHES,
+  });
+
+  if (!result) {
+    return log.plain('Release skipped.');
+  }
+
+  const { type, version } = result.nextRelease;
+
+  log.ok('Released', type, 'version', log.bold(version));
 };
