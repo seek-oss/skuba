3e4d103c61be18df38168fe83227a34a6183a8dd
Ryan Ling <ryan@outlook.com.au>
2020-12-14 13:08:15 +1100

    Implement node wrapper
    See changesets and README for full details.
    
    ---
    
    `skuba node` runs a TypeScript file; it's like a one-off `skuba start`.
    Unlike start, it gives you a REPL when you omit the command line entry
    point rather than falling back to the entry point in `package.json`.
    
    This PR also adds some secret sauce to automatically register the `src`
    module alias when you're running under `skuba node` or `skuba start`.
    This means that you only need a runtime module alias resolver like
    `import skuba-dive/register` at the top of production entry points;
    local scripts can be run with little fanfare using the skuba commands.
    
    ---
    
    Notes:
    
    - I considered calling the command `skuba exec`, but that sounds a bit
      awkward when it supports opening a REPL.
    
    - You may be wondering why don't we just use `tsconfig-paths`.
    
      This is tempting but I'm not convinced that we should depend on
      reading configuration out of `tsconfig.json` at runtime. This can be
      confusing given TypeScript is generally considered a compile-time
      concern, and our own templates only keep `lib` and `node_modules` in
      the runtime Docker image.
    
    - Diving into this has uncovered that the `@babel/node` REPL is pretty
      unusable with modern JavaScript/TypeScript. See the README for more.

---
+++ .changeset/strange-candles-count.md
@@ -0,0 +1,5 @@
+---
+'skuba': patch
+---
+
+**start:** Support `src` module alias
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
+/**
+ * Entry point for the Node.js development API.
+ *
+ * This is where skuba imports point to:
+ *
+ * ```typescript
+ * import { Net } from 'skuba';
+ *
+ * const { Net } = require('skuba');
+ * ```
+ */
+
 export * as Jest from './api/jest';
 export * as Net from './api/net';
