d62dd06e48eb17f805dc6e79ca7f0e3eff350781
damienungseek <damienung@seek.com.au>
2021-04-02 21:09:17 +1100

    add @seek/logger, log hello world only

--- template/lambda-sqs-worker-cdk/src/app.ts
+++ template/lambda-sqs-worker-cdk/src/app.ts
@@ -1,6 +1,10 @@
-/* eslint-disable no-console */
+import createLogger from '@seek/logger';
 import { SQSEvent, SQSHandler } from 'aws-lambda';
 
-export const handler: SQSHandler = (event: SQSEvent) => {
-  console.log(event);
+const logger = createLogger({
+  name: '<%- serviceName %>',
+});
+
+export const handler: SQSHandler = (_: SQSEvent) => {
+  logger.info({ message: 'Hello World!' });
 };
