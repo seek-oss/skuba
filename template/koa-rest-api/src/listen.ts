9a6c449163336a2ad6ce3be7b5d8d8f118b65e7d
Ryan Ling <ryan@outlook.com.au>
2021-07-13 22:28:50 +1000

    Fix listen wrappers

--- template/koa-rest-api/src/listen.ts
+++ template/koa-rest-api/src/listen.ts
@@ -1,13 +1,14 @@
 import './register';
 
 import app from './app';
+import { config } from './config';
 import { rootLogger } from './framework/logging';
 
 // This implements a minimal version of `koa-cluster`'s interface
 // If your application is deployed with more than 1 vCPU you can delete this
 // file and use `koa-cluster` to run `lib/app`.
 
-const listener = app.listen(app.port, () => {
+const listener = app.listen(config.port, () => {
   const address = listener.address();
 
   if (typeof address === 'object' && address) {
