f5f07bdc8fe8915b26658a1676d62ade66665d51
Ryan Ling <ryan@outlook.com.au>
2021-07-13 22:24:25 +1000

    Reduce app boilerplate in API templates
    Closes #352.

--- template/express-rest-api/src/app.ts
+++ template/express-rest-api/src/app.ts
@@ -4,12 +4,9 @@ import express from 'express';
 
 import { healthCheckHandler } from './api/healthCheck';
 import { smokeTestHandler } from './api/smokeTest';
-import { config } from './config';
 
 const app = express()
   .get('/health', healthCheckHandler)
   .get('/smoke', smokeTestHandler);
 
-export default Object.assign(app, {
-  port: config.port,
-});
+export default app;
--- template/koa-rest-api/src/app.ts
+++ template/koa-rest-api/src/app.ts
@@ -1,11 +1,8 @@
 import './register';
 
 import { router } from './api';
-import { config } from './config';
 import { createApp } from './framework/server';
 
 const app = createApp(router.allowedMethods(), router.routes());
 
-export default Object.assign(app, {
-  port: config.port,
-});
+export default app;
