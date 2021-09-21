c33da9c032b2ec8d02eb23435a38e92da4d35b93
Ryan Ling <ryan@outlook.com.au>
2021-09-22 09:42:23 +1000

    Fix bad merge

--- src/utils/dir.ts
+++ src/utils/dir.ts
@@ -39,6 +39,7 @@ export const crawlDirectory = async (
 ) => {
   const ignoreFileFilter = await createInclusionFilter([
     path.join(root, ignoreFilename),
+  ]);
 
   const output = await new FDir()
     .crawlWithOptions(root, {
