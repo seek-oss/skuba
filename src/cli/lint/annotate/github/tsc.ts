89e77853c7b0b12420f48986627e18abee0fe821
Sam Chung <samc@seek.com.au>
2021-10-31 19:41:36 +1100

    chore: add regex comments

--- src/cli/lint/annotate/github/tsc.ts
+++ src/cli/lint/annotate/github/tsc.ts
@@ -1,6 +1,13 @@
 import * as GitHub from '../../../../api/github';
 import { StreamInterceptor } from '../../../../cli/lint/external';
 
+// Regex for typescript output lines. eg.
+// "\x1B[34mtsc      │\x1B[39m src/index.ts(1,2): error TS6133: 'missing' is declared but its value is never read."
+// Pulls out
+// group 1: src/index.ts
+// group 2: 1
+// group 3: 2
+// group 4: TS6133: 'missing' is declared but its value is never read.
 const tscOutputRegex = new RegExp(/([^\s]*)\(([0-9]+),([0-9]+)\): error (.*)/);
 
 export const createTscAnnotations = (
