b448b6c2ad3c056734543c91b360b2642ed54b09
Sam Chung <samc@seek.com.au>
2021-11-01 21:43:30 +1100

    feat: add multi line processing regex

--- src/cli/lint/annotate/github/tsc.ts
+++ src/cli/lint/annotate/github/tsc.ts
@@ -1,20 +1,29 @@
 import * as GitHub from '../../../../api/github';
 import { StreamInterceptor } from '../../../../cli/lint/external';
 
+type tscLevel = 'error' | 'warning' | 'info';
+
+/**
+ * Code from ansi-regex https://github.com/chalk/ansi-regex
+ */
 const ansiPattern = [
   '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
   '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))',
 ].join('|');
 const ansiRegex = new RegExp(ansiPattern, 'g');
 
+/**
+ * Matches the tsc │ prefix on each tsc log
+ */
+const tscPrefixRegex = new RegExp(/(tsc.*?│ )/, 'g');
+
 const tscOutputRegex = new RegExp(
-  /([^\s]*)[\(:](\d+)[,:](\d+)(?:\):\s+|\s+-\s+)(error|warning|info)\s+TS(\d+)\s*:\s*(.*)/,
+  /([^\s]*)[\(:](\d+)[,:](\d+)(?:\):\s+|\s+-\s+)(error|warning|info)\s+TS(\d+)\s*:\s*([\s\S]*?)(?=\n\S)/,
+  'g',
 );
 
-type tscLevels = 'error' | 'warning' | 'info';
-
 const annotationLevelMap: Record<
-  tscLevels,
+  tscLevel,
   GitHub.Annotation['annotation_level']
 > = {
   error: 'failure',
@@ -28,23 +37,23 @@ export const createTscAnnotations = (
 ): GitHub.Annotation[] => {
   const annotations: GitHub.Annotation[] = [];
   if (!tscOk) {
-    const lines = tscOutputStream.output().split('\n').filter(Boolean);
-    lines.forEach((line) => {
-      const plainLine = line.replace(ansiRegex, '');
-      const matches = tscOutputRegex.exec(plainLine);
-      if (matches?.length === 7) {
+    const output = tscOutputStream.output();
+    const rawOutput = output.replace(ansiRegex, '').replace(tscPrefixRegex, '');
+    const matches = rawOutput.matchAll(tscOutputRegex);
+    for (const match of matches) {
+      if (match?.length === 7) {
         annotations.push({
-          annotation_level: annotationLevelMap[matches[4] as tscLevels],
-          path: matches[1],
-          start_line: Number(matches[2]),
-          end_line: Number(matches[2]),
-          start_column: Number(matches[3]),
-          end_column: Number(matches[3]),
-          message: matches[6],
+          annotation_level: annotationLevelMap[match[4] as tscLevel],
+          path: match[1],
+          start_line: Number(match[2]),
+          end_line: Number(match[2]),
+          start_column: Number(match[3]),
+          end_column: Number(match[3]),
+          message: match[6],
           title: `tsc`,
         });
       }
-    });
+    }
   }
 
   return annotations;
