6ee6cb863adf25d81d5b0d9f0919a221006196bd
Sam Chung <samc@seek.com.au>
2021-11-11 20:47:10 +1100

    feat: switch to flatMap

--- src/cli/test/reporters/github/annotations.ts
+++ src/cli/test/reporters/github/annotations.ts
@@ -6,44 +6,42 @@ const jestRegex = /\((.+?):(\d+):(\d+)\)/;
 
 import * as GitHub from '../../../../api/github';
 
-export const createAnnotations = (testResults: TestResult[]) => {
+export const createAnnotations = (
+  testResults: TestResult[],
+): GitHub.Annotation[] => {
   const cwd = process.cwd();
 
-  const annotations: GitHub.Annotation[] = [];
-  testResults.forEach((testResult) => {
+  return testResults.flatMap((testResult) => {
     if (testResult.testExecError) {
-      annotations.push({
+      return {
         annotation_level: 'failure',
         path: path.relative(cwd, testResult.testFilePath),
         start_line: 1,
         end_line: 1,
         message: testResult.testExecError.message,
         title: 'Jest',
-      });
+      };
     }
-
-    if (testResult.numFailingTests === 0) {
-      return;
+    if (testResult.numFailingTests > 0) {
+      return testResult.testResults.flatMap((assertionResult) =>
+        assertionResult.failureMessages.flatMap((failureMessage) => {
+          const match = jestRegex.exec(failureMessage);
+          if (match?.length === 4) {
+            return {
+              annotation_level: 'failure',
+              path: path.relative(cwd, match[1]),
+              start_line: Number(match[2]),
+              end_line: Number(match[2]),
+              start_column: Number(match[3]),
+              end_column: Number(match[3]),
+              message: failureMessage,
+              title: 'Jest',
+            };
+          }
+          return [];
+        }),
+      );
     }
-
-    testResult.testResults.forEach((assertionResult) => {
-      assertionResult.failureMessages.forEach((failureMessage) => {
-        const match = jestRegex.exec(failureMessage);
-        if (match?.length === 4) {
-          annotations.push({
-            annotation_level: 'failure',
-            path: path.relative(cwd, match[1]),
-            start_line: Number(match[2]),
-            end_line: Number(match[2]),
-            start_column: Number(match[3]),
-            end_column: Number(match[3]),
-            message: failureMessage,
-            title: 'Jest',
-          });
-        }
-      });
-    });
+    return [];
   });
-
-  return annotations;
 };
