253f718f545398f21c22c99a931cc56d2b305e05
Ryan Ling <ryan@outlook.com.au>
2021-11-17 05:31:58 +1100

    Clean up a couple testing things
    - Mock out console logging in a test module
    - Set `coveragePathIgnorePatterns` in our configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,8 @@
 import { Jest } from './src';
 
 export default Jest.mergePreset({
+  coveragePathIgnorePatterns: ['<rootDir>/integration/', '<rootDir>/template/'],
+  displayName: 'unit',
   setupFiles: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: [
     '<rootDir>/template/',
