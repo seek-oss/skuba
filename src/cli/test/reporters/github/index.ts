a130b4c4c22ccb09dfbd3cef2df5a8f97478abd8
Ryan Ling <ryan@outlook.com.au>
2021-11-17 08:29:57 +1100

    Report on test coverage

--- src/cli/test/reporters/github/index.ts
+++ src/cli/test/reporters/github/index.ts
@@ -1,25 +1,30 @@
 import type { Context, Reporter } from '@jest/reporters';
 import type { AggregatedResult } from '@jest/test-result';
+import stripAnsi from 'strip-ansi';
 
+import * as Buildkite from '../../../../api/buildkite';
 import * as GitHub from '../../../../api/github';
 import {
   buildNameFromEnvironment,
   enabledFromEnvironment,
 } from '../../../../api/github/environment';
 import { log } from '../../../../utils/logging';
+import { renderCoverageText } from '../coverage';
 
 import { generateAnnotationEntries } from './annotations';
 
 export default class GitHubReporter implements Pick<Reporter, 'onRunComplete'> {
   async onRunComplete(
     _contexts: Set<Context>,
-    { testResults }: AggregatedResult,
+    { coverageMap, testResults }: AggregatedResult,
   ): Promise<void> {
     if (!enabledFromEnvironment()) {
       return;
     }
 
     try {
+      const coverage = renderCoverageText(coverageMap);
+
       const entries = generateAnnotationEntries(testResults);
 
       const build = buildNameFromEnvironment();
@@ -35,11 +40,29 @@ export default class GitHubReporter implements Pick<Reporter, 'onRunComplete'> {
           ? '`skuba test` passed.'
           : '`skuba test` found issues that require triage.';
 
+        if (coverage) {
+          const buildkiteOutput: string = [
+            '`skuba test` coverage:',
+            Buildkite.md.terminal(coverage),
+          ].join('\n\n');
+
+          await Buildkite.annotate(buildkiteOutput, {
+            context: `skuba-test-${
+              displayName ? `-${Buffer.from(displayName).toString('hex')}` : ''
+            }`,
+            scopeContextToStep: true,
+            style: isOk ? 'success' : 'error',
+          });
+        }
+
         await GitHub.createCheckRun({
           name,
           annotations,
           conclusion: isOk ? 'success' : 'failure',
           summary,
+          text: coverage
+            ? Buildkite.md.terminal(stripAnsi(coverage))
+            : undefined,
           title: `${build} ${isOk ? 'passed' : 'failed'}`,
         });
       }
