9408d5ccae997e01c2725e7e1047d7119976e6a3
Sam Chung <samc@seek.com.au>
2021-11-16 15:33:10 +1100

    Separate git utils from checkRun (#670)
    We need to use the owner/repo function elsewhere. Separated the common
    functions we may use elsewhere into their own file in the utils folder.

--- src/api/github/checkRun.ts
+++ src/api/github/checkRun.ts
@@ -1,8 +1,7 @@
 import { Octokit } from '@octokit/rest';
 import type { Endpoints } from '@octokit/types';
-import fs from 'fs-extra';
-import git from 'isomorphic-git';
 
+import { getHeadSha, getOwnerRepo } from '../../utils/git';
 import { pluralise } from '../../utils/logging';
 
 type Output = NonNullable<
@@ -12,48 +11,6 @@ type Output = NonNullable<
 export type Annotation = NonNullable<Output['annotations']>[number];
 
 const GITHUB_MAX_ANNOTATIONS = 50;
-
-/**
- * Matches the owner and repository names in a GitHub repository URL.
- *
- * For example, given the following input strings:
- *
- * ```console
- * git@github.com:seek-oss/skuba.git
- * https://github.com/seek-oss/skuba.git
- * ```
- *
- * This pattern will produce the following matches:
- *
- * 1. seek-oss
- * 2. skuba
- */
-const ownerRepoRegex = /github.com(?::|\/)(.+)\/(.+).git$/;
-
-const getOwnerRepo = async (
-  dir: string,
-): Promise<{ owner: string; repo: string }> => {
-  const remotes = await git.listRemotes({ dir, fs });
-
-  for (const { url } of remotes) {
-    const match = ownerRepoRegex.exec(url);
-
-    const owner = match?.[1];
-    const repo = match?.[2];
-
-    if (owner && repo) {
-      return { owner, repo };
-    }
-  }
-
-  throw new Error('Could not find a GitHub remote');
-};
-
-const getHeadSha = async (dir: string): Promise<string> => {
-  const [commit] = await git.log({ depth: 1, dir, fs });
-
-  return commit.oid;
-};
 
 /**
  * Suffixes the title with the number of annotations added, e.g.
