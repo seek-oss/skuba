945079ea68e5a9f14acb9a8e5e0a162a95753136
Sam Chung <samc@seek.com.au>
2021-11-16 17:17:25 +1100

    feat: commit raw files

---
+++ src/cli/changeset/readChangesetState.ts
@@ -0,0 +1,27 @@
+import { readPreState } from '@changesets/pre';
+import readChangesets from '@changesets/read';
+import type { NewChangeset, PreState } from '@changesets/types';
+
+export type ChangesetState = {
+  preState: PreState | undefined;
+  changesets: NewChangeset[];
+};
+
+export default async function readChangesetState(
+  cwd: string = process.cwd(),
+): Promise<ChangesetState> {
+  const preState = await readPreState(cwd);
+  const isInPreMode = preState !== undefined && preState.mode === 'pre';
+
+  let changesets = await readChangesets(cwd);
+
+  if (isInPreMode) {
+    const changesetsToFilter = new Set(preState.changesets);
+    changesets = changesets.filter((x) => !changesetsToFilter.has(x.id));
+  }
+
+  return {
+    preState: isInPreMode ? preState : undefined,
+    changesets,
+  };
+}
