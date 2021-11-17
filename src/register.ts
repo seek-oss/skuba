c09c831c0b8475caf85c4b6e667e45df5f92dc82
Ryan Ling <ryan@outlook.com.au>
2021-11-17 12:13:53 +1100

    Register `tsconfig-paths` for development (#678)
    Note that this does not modify our typical `skuba-dive/register` runtime
    hook. The drift between the two may be a bit puzzling in the time being,
    but it's somewhat rare for production applications to include
    `tsconfig.json` in their final bundle or container. Alignment should
    eventually come as we investigate shifting over to `esbuild`, which
    supports compile-time `tsconfig-paths` resolution.

--- src/register.ts
+++ src/register.ts
@@ -13,37 +13,11 @@
  * Production code should be compiled down to JavaScript and run with Node.js.
  *
  * For equivalent module alias and source map support in production,
- * import the `skuba-dive/register` hook.
+ * import the `skuba-dive/register` and/or `tsconfig-paths/register` hook.
  *
  * {@link https://github.com/seek-oss/skuba-dive#register}
  */
 
 import 'source-map-support/register';
 
-import path from 'path';
-
-import { addAlias } from 'module-alias';
-import readPkgUp from 'read-pkg-up';
-
-import { log } from './utils/logging';
-
-const registerModuleAliases = () => {
-  // This may pick the wrong `package.json` if we are in a nested directory.
-  // Consider revisiting this when we decide how to better support monorepos.
-  const result = readPkgUp.sync();
-
-  if (result === undefined) {
-    log.warn(log.bold('src'), '→', 'not found');
-
-    return;
-  }
-
-  const root = path.dirname(result.path);
-  const src = path.join(root, 'src');
-
-  log.subtle(log.bold('src'), '→', src);
-
-  addAlias('src', src);
-};
-
-registerModuleAliases();
+import 'tsconfig-paths/register';
