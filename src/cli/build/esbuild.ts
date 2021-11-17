b91437fb3f797c624c143f019e03fa9217db82b2
Ryan Ling <ryan@outlook.com.au>
2021-11-17 15:11:54 +1100

    Add a tool switch

---
+++ src/cli/build/esbuild.ts
@@ -0,0 +1,7 @@
+import { log } from '../../utils/logging';
+
+export const esbuild = async () => {
+  await Promise.resolve();
+
+  log.subtle('Coming soon!');
+};
--- src/cli/build/index.ts
+++ src/cli/build/index.ts
@@ -1,3 +1,37 @@
-import { tsc } from './tsc';
+import chalk from 'chalk';
 
-export const build = tsc;
+import { log } from '../../utils/logging';
+import { getStringPropFromConsumerManifest } from '../../utils/manifest';
+
+export const build = async () => {
+  const tool = await getStringPropFromConsumerManifest('build');
+
+  switch (tool) {
+    case 'esbuild': {
+      log.plain(chalk.yellow('esbuild'));
+      const { esbuild } = await import('./esbuild');
+      await esbuild();
+      return;
+    }
+
+    // TODO: flip the default case over to `esbuild` in skuba 4.
+    case undefined:
+    case 'tsc': {
+      log.plain(chalk.blue('tsc'));
+      const { tsc } = await import('./tsc');
+      await tsc();
+      return;
+    }
+
+    default: {
+      log.err(
+        'We don’t support the build tool specified in your',
+        log.bold('package.json'),
+        'yet:',
+      );
+      log.err(log.subtle(JSON.stringify({ skuba: { build: tool } }, null, 2)));
+      process.exitCode = 1;
+      return;
+    }
+  }
+};
