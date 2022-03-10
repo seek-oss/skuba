dccc0e6835a520ff192ae8a113ac1f347bf9f8e1
Ryan Ling <ryan@outlook.com.au>
2022-03-11 08:28:01 +1100

    Remove transpiled `.js` files

--- template/hello-world-example/src/framework/middleware.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-exports.__esModule = true;
-exports.jsonBodyParser = void 0;
-var koa_bodyparser_1 = require('koa-bodyparser');
-exports.jsonBodyParser = (0, koa_bodyparser_1['default'])({
-  enableTypes: ['json'],
-});
