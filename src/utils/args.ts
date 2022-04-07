5601b73121108850f6f249ef7485bc7f27fc34b6
Ryan Ling <ryan@outlook.com.au>
2022-04-07 23:53:35 +1000

    Update src/utils/args.ts

--- src/utils/args.ts
+++ src/utils/args.ts
@@ -10,10 +10,8 @@ export const hasSerialFlag = (args = process.argv, env = process.env) =>
   Boolean(
     // Run serially on SEEK's central npm publishing pipeline.
     // Exhausting agents here can cause grief.
-    
-    env.BUILDKITE_AGENT_META_DATA_QUEUE?.split(',').reduce(
-      (accumulator, currentValue) => accumulator || currentValue.includes('artefacts:npm'),
-      false,
+    env.BUILDKITE_AGENT_META_DATA_QUEUE?.split(',').some(
+      (queueName) => queueName.startsWith('artefacts:npm')
     ),
   );
 
