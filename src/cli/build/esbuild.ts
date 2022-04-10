fd0b26011424e4038a494b4d8682e09ba9b8779c
Ryan Ling <ryan@outlook.com.au>
2022-04-10 13:32:23 +1000

    Read actual `tsconfig.build.json`

--- src/cli/build/esbuild.ts
+++ src/cli/build/esbuild.ts
@@ -1,62 +1,94 @@
-import path from 'path';
+import { inspect } from 'util';
 
 import tsconfigPaths from '@esbuild-plugins/tsconfig-paths';
 import { build } from 'esbuild';
+import type { TsConfigJson } from 'type-fest';
+import ts, { ModuleKind, ModuleResolutionKind } from 'typescript';
 
-import { crawlDirectory } from '../../utils/dir';
+import { log } from '../../utils/logging';
 
-const SUPPORTED_FILE_EXTENSIONS = new Set([
-  '.cjs',
-  '.cts',
-  '.js',
-  '.json',
-  '.jsx',
-  '.mjs',
-  '.mts',
-  '.ts',
-  '.tsx',
-]);
+const formatHost: ts.FormatDiagnosticsHost = {
+  getCanonicalFileName: (path) => path,
+  getCurrentDirectory: ts.sys.getCurrentDirectory.bind(undefined),
+  getNewLine: () => ts.sys.newLine,
+};
 
 export const esbuild = async () => {
+  const currentDir = process.cwd();
+
   // TODO: support `--project` option
   const tsconfigFilepath = 'tsconfig.build.json';
 
-  // TODO: read actual `tsconfig.json`
-  const tsconfig = {
-    compilerOptions: {
-      baseUrl: '.',
-      declaration: true,
-      outDir: 'lib',
-      paths: {
-        src: ['src'],
-      },
-    },
-    include: ['src/**/*'],
-  };
+  const tsconfigFile = ts.findConfigFile(
+    currentDir,
+    ts.sys.fileExists.bind(undefined),
+    tsconfigFilepath,
+  );
+  if (!tsconfigFile) {
+    log.err(`Could not find ${tsconfigFilepath}.`);
+    process.exitCode = 1;
+    return;
+  }
 
-  // TODO: translate `tsconfig.json#/include`
-  const allFiles = await crawlDirectory(path.join(process.cwd(), 'src'));
+  const readConfigFile = ts.readConfigFile(
+    tsconfigFile,
+    ts.sys.readFile.bind(undefined),
+  );
+  if (readConfigFile.error) {
+    log.err(`Could not read ${tsconfigFilepath}.`);
+    log.subtle(ts.formatDiagnostic(readConfigFile.error, formatHost));
+    process.exitCode = 1;
+    return;
+  }
 
-  const files = allFiles.filter((file) =>
-    SUPPORTED_FILE_EXTENSIONS.has(path.extname(file)),
+  const tsconfig = readConfigFile.config as TsConfigJson;
+
+  const parsedCommandLine = ts.parseJsonConfigFileContent(
+    tsconfig,
+    ts.sys,
+    currentDir,
   );
 
-  const buildResult = await build({
+  if (parsedCommandLine.errors.length) {
+    log.err(`Could not parse ${tsconfigFilepath}.`);
+    log.subtle(ts.formatDiagnostics(parsedCommandLine.errors, formatHost));
+    process.exitCode = 1;
+    return;
+  }
+
+  const { fileNames: entryPoints, options: compilerOptions } =
+    parsedCommandLine;
+
+  // TODO: propagate debug logger
+  log.debug('Files');
+  entryPoints.forEach((filepath) => log.debug(filepath));
+
+  log.debug('Compiler options');
+  log.debug(inspect(compilerOptions));
+
+  // TODO: do we need to check both of these?
+  const isNode =
+    compilerOptions.module === ModuleKind.CommonJS ||
+    compilerOptions.moduleResolution === ModuleResolutionKind.NodeJs;
+
+  await build({
     // TODO: make these configurable
     bundle: false,
     sourcemap: true,
 
-    entryPoints: files,
-    outdir: tsconfig.compilerOptions.outDir,
+    // TODO: investigate incremental builds
+    // incremental: true
+
+    // TODO: figure out CJS vs ESM
+    ...(isNode ? { format: 'cjs', platform: 'node' } : null),
+
+    entryPoints,
+    outdir: compilerOptions.outDir,
     plugins: [tsconfigPaths({ tsconfig })],
     tsconfig: tsconfigFilepath,
   });
 
-  if (buildResult.errors.length) {
-    process.exitCode = 1;
-  }
-
-  if (tsconfig.compilerOptions.declaration) {
+  if (compilerOptions.declaration) {
     const { tsc } = await import('./tsc');
 
     await tsc(['--declaration', '--emitDeclarationOnly']);
