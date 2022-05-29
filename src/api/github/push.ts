8b05e981927c09134cd3dce6d2f45bf9e9a822af
Sam Chung <samchungy@gmail.com>
2022-05-29 11:52:50 +1000

    add new graphql

---
+++ src/api/github/push.ts
@@ -0,0 +1,147 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+import { graphql } from '@octokit/graphql';
+import type { FileAddition, FileDeletion } from '@octokit/graphql-schema';
+
+import type { ChangedFile } from '../git/getChangedFiles';
+import { getChangedFiles } from '../git/getChangedFiles';
+import { getHeadCommitId } from '../git/log';
+import { getOwnerAndRepo } from '../git/remote';
+
+import { apiTokenFromEnvironment } from './environment';
+
+interface CreateCommitResult {
+  createCommitOnBranch: {
+    commit: {
+      id: string;
+    };
+  };
+}
+
+interface CommitAndPushAllChangesParams {
+  dir: string;
+  branch: string;
+  messageHeadline: string;
+  messageBody?: string;
+}
+
+export const commitAndPushAllChanges = async ({
+  dir,
+  branch,
+  messageHeadline,
+  messageBody,
+}: CommitAndPushAllChangesParams) => {
+  const changedFiles = await getChangedFiles({ dir });
+  const fileChanges = await mapChangedFilesToFileChanges(dir, changedFiles);
+
+  await commitAndPush({
+    dir,
+    branch,
+    messageHeadline,
+    messageBody,
+    fileChanges,
+  });
+};
+
+interface FileChanges {
+  additions: FileAddition[];
+  deletions: FileDeletion[];
+}
+
+export const mapChangedFilesToFileChanges = async (
+  dir: string,
+  changedFiles: ChangedFile[],
+): Promise<FileChanges> => {
+  const { added, deleted } = changedFiles.reduce<{
+    added: string[];
+    deleted: string[];
+  }>(
+    (files, changedFile) => {
+      const filePath = changedFile.path;
+      if (changedFile.state === 'deleted') {
+        files.deleted.push(filePath);
+      } else {
+        files.added.push(filePath);
+      }
+
+      return files;
+    },
+    { added: [], deleted: [] },
+  );
+
+  const additions: FileAddition[] = await Promise.all(
+    added.map(async (filePath) => ({
+      path: filePath,
+      contents: (
+        await fs.readFile(path.join(dir, filePath))
+      ).toString('base64'),
+    })),
+  );
+
+  const deletions: FileDeletion[] = deleted.map((filePath) => ({
+    path: filePath,
+  }));
+
+  return {
+    additions,
+    deletions,
+  };
+};
+
+interface CommitAndPushParams {
+  dir: string;
+  branch: string;
+  messageHeadline: string;
+  messageBody?: string;
+  fileChanges: FileChanges;
+}
+
+export const commitAndPush = async ({
+  dir,
+  branch,
+  messageHeadline,
+  messageBody,
+  fileChanges,
+}: CommitAndPushParams) => {
+  const [{ owner, repo }, headCommitId] = await Promise.all([
+    getOwnerAndRepo({ dir }),
+    getHeadCommitId({ dir }),
+  ]);
+
+  await graphql<CreateCommitResult>(
+    `
+      mutation Mutation($input: CreateCommitOnBranchInput!) {
+        createCommitOnBranch(input: $input) {
+          commit {
+            id
+          }
+        }
+      }
+    `,
+    {
+      input: {
+        branch: {
+          repositoryNameWithOwner: `${owner}/${repo}`,
+          branchName: branch,
+        },
+        message: {
+          headline: messageHeadline,
+          body: messageBody ?? null,
+        },
+        expectedHeadOid: headCommitId,
+        clientMutationId: 'skuba',
+        fileChanges,
+      },
+      headers: {
+        authorization: `bearer ${apiTokenFromEnvironment() || ''}`,
+      },
+    },
+  );
+};
+
+commitAndPushAllChanges({
+  dir: process.cwd(),
+  branch: 'graphql-commit',
+  messageHeadline: 'add new graphql',
+}).catch(console.error);
