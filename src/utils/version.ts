seek-oss/skuba

commit 43b9ebda2eb77090080ba1a415c8e56c8a1666c2

Fix CI (#962)

src/utils/version.ts
@@ -1,15 +1,45 @@
-import packageJson from 'package-json';
+import path from 'path';
+
+import validatePackageName from 'validate-npm-package-name';
 
 import { getSkubaManifest } from './manifest';
+import { isObject } from './validation';
 import { withTimeout } from './wait';
+
+const loadPackageJson = async (
+  packageName: string,
+): Promise<Record<string, unknown>> => {
+  const { validForNewPackages } = validatePackageName(packageName);
+
+  if (!validForNewPackages) {
+    throw new Error(`Package "${packageName}" does not have a valid name`);
+  }
+
+  const message = `Package "${packageName}" does not have a valid package.json manifest`;
+
+  let packageJson: unknown;
+  try {
+    packageJson = await import(path.posix.join(packageName, 'package.json'));
+  } catch {
+    throw new Error(message);
+  }
+
+  if (!isObject(packageJson)) {
+    throw new Error(message);
+  }
+
+  return packageJson;
+};
 
 export const latestNpmVersion = async (
   packageName: string,
 ): Promise<string> => {
-  const { version } = await packageJson(packageName);
+  const { version } = await loadPackageJson(packageName);
 
   if (typeof version !== 'string') {
-    throw new Error(`No version found for package "${packageName}"`);
+    throw new Error(
+      `Package "${packageName}" does not have a valid version in its package.json manifest`,
+    );
   }
 
   return version;

commit 89280b56ae00f934958f391684cbc20cd31dbb46

configure: Fix package version lookups
I unwittingly broke this in #962 and unfortunately we have no coverage
of the undercooked `configure` command currently. That rushed hack
doesn't make any sense in hindsight as it tries to fetch the "latest"
version from the local disk, which isn't going to tell you if local is
outdated.

src/utils/version.ts
@@ -1,48 +1,27 @@
-import path from 'path';
-
+import searchNpm from 'libnpmsearch';
 import validatePackageName from 'validate-npm-package-name';
 
 import { getSkubaManifest } from './manifest';
-import { isObject } from './validation';
 import { withTimeout } from './wait';
 
-const loadPackageJson = async (
+export const latestNpmVersion = async (
   packageName: string,
-): Promise<Record<string, unknown>> => {
+): Promise<string> => {
   const { validForNewPackages } = validatePackageName(packageName);
 
   if (!validForNewPackages) {
     throw new Error(`Package "${packageName}" does not have a valid name`);
   }
 
-  const message = `Package "${packageName}" does not have a valid package.json manifest`;
+  const [result] = await searchNpm(packageName, { limit: 1, timeout: 5_000 });
 
-  let packageJson: unknown;
-  try {
-    packageJson = await import(path.posix.join(packageName, 'package.json'));
-  } catch {
-    throw new Error(message);
-  }
-
-  if (!isObject(packageJson)) {
-    throw new Error(message);
-  }
-
-  return packageJson;
-};
-
-export const latestNpmVersion = async (
-  packageName: string,
-): Promise<string> => {
-  const { version } = await loadPackageJson(packageName);
-
-  if (typeof version !== 'string') {
+  if (result?.name !== packageName) {
     throw new Error(
-      `Package "${packageName}" does not have a valid version in its package.json manifest`,
+      `Package "${packageName}" does not exist on the npm registry`,
     );
   }
 
-  return version;
+  return result.version;
 };
 
 const latestSkubaVersion = async (): Promise<string | null> => {