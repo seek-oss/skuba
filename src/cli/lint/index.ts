f69366845a1bdca287b1c2ab37f0d74978a0f25c
Ryan Ling <ryan@outlook.com.au>
2022-10-11 16:20:14 +1100

    Start on experimental esbuild support (#681)
    Co-authored-by: skuba <34733141+seek-oss-ci@users.noreply.github.com>
    Co-authored-by: Sam Chung <samc@seek.com.au>

--- src/cli/lint/index.ts
+++ src/cli/lint/index.ts
@@ -8,7 +8,7 @@ import { internalLint } from './internal';
 import type { Input } from './types';
 
 export const lint = async (
-  args = process.argv,
+  args = process.argv.slice(2),
   tscOutputStream: Writable | undefined = undefined,
   workerThreads = true,
 ) => {
