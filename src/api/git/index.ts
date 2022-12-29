bf307df107b186f276e861be64782cb45125ab65
Ryan Ling <ryan@outlook.com.au>
2022-12-29 13:38:20 +1100

    Exclude internal files from autofix commits
    Follows on from #1072 and additionally prevents `.npmrc` mishaps.

--- src/api/git/index.ts
+++ src/api/git/index.ts
@@ -1,6 +1,7 @@
 export { commit } from './commit';
 export { commitAllChanges } from './commitAllChanges';
 export { currentBranch } from './currentBranch';
+export type { ChangedFile } from './getChangedFiles';
 export { getChangedFiles } from './getChangedFiles';
 export { getHeadCommitId, getHeadCommitMessage } from './log';
 export { getOwnerAndRepo } from './remote';
