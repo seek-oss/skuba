c8363dfc11e08be954c0dec5a37b37dd9ec0cd86
Ryan Ling <ryan@outlook.com.au>
2023-07-13 09:55:11 +1000

    Try flipping `allowAutomaticSingleRunInference`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,18 @@
+const { ts } = require('eslint-config-seek/extensions');
+
 module.exports = {
   extends: ['skuba'],
+
+  overrides: [
+    {
+      // TypeScript config
+      files: [`**/*.{${ts}}`],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        allowAutomaticSingleRunInference: false,
+      },
+    },
+  ],
   rules: {
     // internal to skuba itself
     'no-process-exit': 'off',
