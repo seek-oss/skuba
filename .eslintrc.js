3787de2d663e6f8608c7508c56cef674870671b2
Ryan Ling <ryan@outlook.com.au>
2023-07-14 10:02:50 +1000

    Add another link

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
       files: [`integration/**/*.{${ts}}`],
       parser: '@typescript-eslint/parser',
       parserOptions: {
+        // seek-oss/eslint-config-seek#124
         // typescript-eslint/typescript-eslint#3851
         allowAutomaticSingleRunInference: false,
       },
