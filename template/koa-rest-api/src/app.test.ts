a708c90a93420a565c8d6f9c61c365e41eb71978
Luke Yuan <22341913+lukeymyuan@users.noreply.github.com>
2023-10-04 16:41:24 +1100

    apply pr feedback

--- template/koa-rest-api/src/app.test.ts
+++ template/koa-rest-api/src/app.test.ts
@@ -10,12 +10,10 @@ describe('app', () => {
 
   it('has a happy health check', () => agent.get('/health').expect(200, ''));
 
-  it('has a reachable smoke test', () =>
-    agent.get('/smoke').expect(({ status }) => {
-      if (status === 404) {
-        throw new Error('Unreachable smoke test');
-      }
-    }));
+  it('has a reachable smoke test', async () => {
+    const response = await agent.get('/smoke');
+    expect(response.status).not.toBe(404);
+  });
 
   /**
    * Test assertion with logic
