4b5d9d203104a27f6237a017623943f3f43435c6
Aaron Moat <2937187+AaronMoat@users.noreply.github.com>
2024-02-06 19:32:03 +1100

    Fix comment

--- src/utils/npmrc.ts
+++ src/utils/npmrc.ts
@@ -1,8 +1,7 @@
-// Preventing against _auth, _authToken, _password
-// https://docs.npmjs.com/cli/v10/configuring-npm/npmrc#auth-related-configuration
-
 export const NPMRC_LINES = ['.npmrc', '!.npmrc', '/.npmrc', '!/.npmrc'];
 
+// Preventing against _auth, _authToken, _password
+// https://docs.npmjs.com/cli/v10/configuring-npm/npmrc#auth-related-configuration
 export const hasNpmrcSecret = (lineOrFullFileContents: string): boolean =>
   lineOrFullFileContents.includes('_auth') ||
   lineOrFullFileContents.includes('_password');
