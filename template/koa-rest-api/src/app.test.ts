6c45022f6be62bbc4bbc42f2fed72f912cce8df0
Ryan Ling <ryan@outlook.com.au>
2024-07-05 15:58:00 +1000

    template/koa-rest-api: Remove `not.toBe(404)`s
    These are super dodgy because they often go unloved as an API evolves
    and end up sending real outbound requests to upstreams when you run the
    test suite. Typically the upstreams don't respond with a 404 so the
    tests still pass, but it's gross.

--- template/koa-rest-api/src/app.test.ts
+++ template/koa-rest-api/src/app.test.ts
@@ -10,16 +10,9 @@ describe('app', () => {
 
   it('has a happy health check', () => agent.get('/health').expect(200, ''));
 
-  it('has a reachable smoke test', async () => {
-    const response = await agent.get('/smoke');
-    expect(response.status).not.toBe(404);
-  });
+  it('has a reachable smoke test', () =>
+    agent.options('/smoke').expect(200, '').expect('Allow', /GET/));
 
-  it('has a reachable nested route', async () => {
-    const response = await agent.get('/jobs');
-    expect(response.status).not.toBe(404);
-  });
-
-  it('has OPTIONS for a nested route', () =>
-    agent.options('/jobs').expect(200).expect('allow', /HEAD/));
+  it('has a reachable nested route', () =>
+    agent.options('/jobs').expect(200, '').expect('Allow', /POST/));
 });
