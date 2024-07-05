f5c44660ea13ea48efe7a12024542c955ccd887c
Ryan Ling <ryan@outlook.com.au>
2024-07-05 16:01:06 +1000

    Tweak

--- template/koa-rest-api/src/app.test.ts
+++ template/koa-rest-api/src/app.test.ts
@@ -11,8 +11,10 @@ describe('app', () => {
   it('has a happy health check', () => agent.get('/health').expect(200, ''));
 
   it('has a reachable smoke test', () =>
-    agent.options('/smoke').expect(200, '').expect('Allow', /GET/));
+    agent.options('/smoke').expect(200, '').expect('Allow', 'HEAD, GET'));
 
   it('has a reachable nested route', () =>
     agent.options('/jobs').expect(200, '').expect('Allow', /POST/));
+
+  it('handles an unknown route', () => agent.options('/admin.php').expect(404));
 });
