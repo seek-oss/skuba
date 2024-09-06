3695627dbf8292b67556806e469116e9463f2d3a
Geordie Elliot-Kerr <65689460+GeordieEK@users.noreply.github.com>
2024-09-06 15:21:26 +1000

    Fix: Truncate Buildkite Annotation (#1645)
    Co-authored-by: Aaron Moat <2937187+AaronMoat@users.noreply.github.com>

--- src/api/buildkite/annotate.test.ts
+++ src/api/buildkite/annotate.test.ts
@@ -1,9 +1,11 @@
 import * as execModule from '../../utils/exec';
+import { log } from '../../utils/logging';
 
-import { annotate } from './annotate';
+import { MAX_SIZE, TRUNCATION_WARNING, annotate } from './annotate';
 
 const exec = jest.spyOn(execModule, 'exec');
 const hasCommand = jest.spyOn(execModule, 'hasCommand');
+const mockWarn = jest.spyOn(log, 'warn').mockImplementation(() => undefined);
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -28,6 +30,8 @@ const setEnvironmentVariables = () => {
 
 describe('annotate', () => {
   const markdown = '**Message**';
+  const endOfMessage = 'EndMessage';
+  const oversizeMarkdown = 'a'.repeat(MAX_SIZE).concat(endOfMessage);
 
   describe.each`
     description                    | opts
@@ -52,6 +56,43 @@ describe('annotate', () => {
       await expect(annotate(markdown)).resolves.toBeUndefined();
 
       expect(exec).not.toHaveBeenCalled();
+    });
+
+    it('warns about truncation when annotation exceeds the maximum size', async () => {
+      setEnvironmentVariables();
+      await annotate(oversizeMarkdown);
+
+      const lastCall = exec.mock.calls[exec.mock.calls.length - 1];
+      if (!lastCall) {
+        throw new Error('Expected exec to have been called at least once');
+      }
+
+      const lastArgument = lastCall[lastCall.length - 1];
+
+      if (typeof lastArgument !== 'string') {
+        throw new Error('Expected the last argument to be a string');
+      }
+
+      expect(lastArgument.endsWith(TRUNCATION_WARNING)).toBe(true);
+    });
+
+    it('logs the full message when annotation is truncated', async () => {
+      setEnvironmentVariables();
+      await annotate(oversizeMarkdown);
+
+      const lastCall = mockWarn.mock.calls[exec.mock.calls.length - 1];
+      if (!lastCall) {
+        throw new Error('Expected log.warn to have been called at least once');
+      }
+
+      const lastArgument = lastCall[lastCall.length - 1];
+
+      if (typeof lastArgument !== 'string') {
+        throw new Error('Expected the last argument to be a string');
+      }
+
+      // Check for the end of message in case there's a failure, entire message isn't printed (it's too large)
+      expect(lastArgument.endsWith(endOfMessage)).toBe(true);
     });
 
     it('skips when `buildkite-agent` is not present', async () => {
