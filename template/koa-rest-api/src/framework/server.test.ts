5fd488d5bb3ab37be2b7f99a068988db675d411c
Aaron Moat <2937187+AaronMoat@users.noreply.github.com>
2024-09-28 12:41:25 +1000

    Disable @typescript-eslint/only-throw-error in test files (#1689)

--- template/koa-rest-api/src/framework/server.test.ts
+++ template/koa-rest-api/src/framework/server.test.ts
@@ -203,7 +203,6 @@ describe('createApp', () => {
 
   it('handles null error', async () => {
     middleware.mockImplementation(() => {
-      /* eslint-disable-next-line @typescript-eslint/only-throw-error */
       throw null;
     });
 
