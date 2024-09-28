d8ced2ad0e00c7fcb1d5a01f37eeb3792f0494f2
Sam Chung <samc@seek.com.au>
2024-09-28 11:35:34 +1000

    Switch docker image source to Public ECR (#1684)
    Co-authored-by: skuba <34733141+seek-oss-ci@users.noreply.github.com>

--- src/cli/lint/internalLints/upgrade/patches/8.2.1/index.ts
+++ src/cli/lint/internalLints/upgrade/patches/8.2.1/index.ts
@@ -2,6 +2,7 @@ import type { Patches } from '../..';
 
 import { tryCollapseDuplicateMergeKeys } from './collapseDuplicateMergeKeys';
 import { tryPatchDockerComposeFiles } from './patchDockerCompose';
+import { tryPatchDockerImages } from './patchDockerImages';
 import { tryUpgradeESLint } from './upgradeESLint';
 
 export const patches: Patches = [
@@ -16,5 +17,10 @@ export const patches: Patches = [
   {
     apply: tryPatchDockerComposeFiles,
     description: 'Remove version field from docker-compose files',
+  },
+  {
+    apply: tryPatchDockerImages,
+    description:
+      'Update docker image references to use public.ecr.aws and remove --platform flag',
   },
 ];
