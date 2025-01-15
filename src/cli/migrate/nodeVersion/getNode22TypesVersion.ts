88e54f7b9aab3ab0b8ab470af4b89e7b6433c51e
Zac Brydon <52645024+zbrydon@users.noreply.github.com>
2025-01-15 15:58:59 +1100

    Remove " from node types version

--- src/cli/migrate/nodeVersion/getNode22TypesVersion.ts
+++ src/cli/migrate/nodeVersion/getNode22TypesVersion.ts
@@ -1,6 +1,6 @@
 import { execSync } from 'child_process';
 
 export const getNode22TypesVersion = (major: number) =>
-  execSync(
-    `npm show @types/node@^${major} version --json | jq '.[-1]'`,
-  ).toString();
+  execSync(`npm show @types/node@^${major} version --json | jq '.[-1]'`)
+    .toString()
+    .replace(/"/g, '');
