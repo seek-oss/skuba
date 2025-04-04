69ad8323e5850db192eba4dcfc9a76c8558089a1
Aaron Moat <2937187+AaronMoat@users.noreply.github.com>
2025-04-04 17:14:01 +1100

    Stop bundling in tests (#1844)
    Co-authored-by: Ryan Ling <ryan@outlook.com.au>

---
+++ .changeset/new-cars-fix.md
@@ -0,0 +1,5 @@
+---
+'skuba': patch
+---
+
+template/lambda-sqs-worker-cdk: Update test to skip esbuild bundling by using the AWS CDK context key `aws:cdk:bundling-stacks`
---
+++ src/cli/lint/internalLints/upgrade/patches/10.1.0/stopBundlingInCDKTests.ts
@@ -0,0 +1,85 @@
+import { inspect } from 'util';
+
+import { glob } from 'fast-glob';
+import { promises as fs } from 'fs-extra';
+
+import type { PatchFunction, PatchReturnType } from '../..';
+import { log } from '../../../../../../utils/logging';
+
+const fetchFiles = async (files: string[]) =>
+  Promise.all(
+    files.map(async (file) => {
+      const contents = await fs.readFile(file, 'utf8');
+
+      return {
+        file,
+        contents,
+      };
+    }),
+  );
+
+export const hasAppImportRegex =
+  /import\s*\{\s*(?:[^}]*,\s*)?App\s*(?:,\s*[^}]*)?\s*\}\s*from\s*['"]aws-cdk-lib['"]/gm;
+
+const addBundlingContext = (contents: string) => {
+  if (contents.includes('new App()') && hasAppImportRegex.test(contents)) {
+    return contents.replaceAll(
+      'new App()',
+      "new App({ context: { 'aws:cdk:bundling-stacks': [] } })",
+    );
+  }
+
+  return contents;
+};
+
+const stopBundlingInCDKTests: PatchFunction = async ({
+  mode,
+}): Promise<PatchReturnType> => {
+  const infraTestFileNames = await glob(['**/infra/**/*.test.ts']);
+
+  if (!infraTestFileNames.length) {
+    return {
+      result: 'skip',
+      reason: 'no CDK test files found',
+    };
+  }
+
+  const infraTestFiles = await fetchFiles(infraTestFileNames);
+
+  const mapped = infraTestFiles.map(({ file, contents }) => ({
+    file,
+    before: contents,
+    after: addBundlingContext(contents),
+  }));
+
+  if (!mapped.some(({ before, after }) => before !== after)) {
+    return {
+      result: 'skip',
+      reason: 'no CDK test files need patching',
+    };
+  }
+
+  if (mode === 'lint') {
+    return {
+      result: 'apply',
+    };
+  }
+
+  await Promise.all(
+    mapped.map(async ({ file, after }) => {
+      await fs.writeFile(file, after);
+    }),
+  );
+
+  return { result: 'apply' };
+};
+
+export const tryStopBundlingInCDKTests: PatchFunction = async (config) => {
+  try {
+    return await stopBundlingInCDKTests(config);
+  } catch (err) {
+    log.warn('Failed to remove bundling in CDK tests');
+    log.subtle(inspect(err));
+    return { result: 'skip', reason: 'due to an error' };
+  }
+};
