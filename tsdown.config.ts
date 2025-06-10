c980001e5851cd38739292e0ac99f61a9ed0a8a1
samchungy <samchungy@gmail.com>
2025-06-10 13:29:23 +1000

    oops

--- tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsdown';
-
-export default defineConfig({
-  platform: 'node',
-  format: ['cjs', 'esm'],
-  dts: true,
-  entry: ['src/index.ts', 'src/skuba.ts'],
-  unbundle: true,
-});
