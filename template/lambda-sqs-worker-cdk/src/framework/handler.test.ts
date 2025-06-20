172d5ad601a688183ab2fa2c40819675a9b3ac72
samchungy <samchungy@gmail.com>
2025-06-20 18:32:59 +1000

    Fix test

--- template/lambda-sqs-worker-cdk/src/framework/handler.test.ts
+++ template/lambda-sqs-worker-cdk/src/framework/handler.test.ts
@@ -32,7 +32,7 @@ describe('createHandler', () => {
       {
         awsRequestId: '-',
         level: 20,
-        msg: 'Function succeeded',
+        msg: 'Function completed',
       },
     ]);
   });
