0cfe0362df482be13fe7595cd59941e4d5e9c4b9
samchungy <samchungy@gmail.com>
2025-07-01 13:57:15 +1000

    add simple tests

---
+++ packages/eslint-config-skuba/requireExtensions.test.ts
@@ -0,0 +1,17 @@
+/* eslint-disable import-x/order */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable import-x/no-duplicates */
+
+// This file is used to test the requireExtensions rule
+
+// eslint-disable-next-line require-extensions/require-extensions
+import { test as simpleTest } from './test/simple';
+import { test as validSimpleTest } from './test/simple.js';
+
+// eslint-disable-next-line require-extensions/require-index
+import { test as indexFileTest } from './test/indexFile';
+import { test as validIndexFileTest } from './test/indexFile/index.js';
+
+// eslint-disable-next-line require-extensions/require-extensions
+import { test as srcTest } from 'src/file';
+import { test as validSrcTest } from 'src/file.js';
