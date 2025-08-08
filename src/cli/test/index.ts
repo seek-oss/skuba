ab477f0f6b80b24f651a4413300cb8cd0bc0a515
samchungy <samchungy@gmail.com>
2025-08-08 21:04:43 +1000

    it sets test

--- src/cli/test/index.ts
+++ src/cli/test/index.ts
@@ -3,14 +3,10 @@ import { createExec } from '../../utils/exec.js';
 export const test = async () => {
   const argv = process.argv.slice(2);
 
-  // Prepare environment variables for Jest
   const nodeOptions = process.env.NODE_OPTIONS ?? '';
 
   const execWithEnv = createExec({
     env: {
-      // This is usually set in `jest-cli`'s binary wrapper
-      NODE_ENV: process.env.NODE_ENV ?? 'test',
-
       // ts-jest is logging a warning about `isolatedModules`.
       // This is a workaround until we can remove the `isolatedModules` option.
       // https://github.com/seek-oss/skuba/issues/1841
