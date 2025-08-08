e0ffcbea13a92735898fbd3dd9d962964112e673
samchungy <samchungy@gmail.com>
2025-08-08 21:07:02 +1000

    remove other log env var

--- src/cli/test/index.ts
+++ src/cli/test/index.ts
@@ -7,11 +7,6 @@ export const test = async () => {
 
   const execWithEnv = createExec({
     env: {
-      // ts-jest is logging a warning about `isolatedModules`.
-      // This is a workaround until we can remove the `isolatedModules` option.
-      // https://github.com/seek-oss/skuba/issues/1841
-      TS_JEST_LOG: process.env.TS_JEST_LOG ?? 'stdout:error',
-
       NODE_OPTIONS: !nodeOptions?.includes('--experimental-vm-modules')
         ? `${nodeOptions} --experimental-vm-modules`
         : nodeOptions,
