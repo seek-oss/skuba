ee2d646c9e2892a10fb441a4f886c661079cb199
samchungy <samchungy@gmail.com>
2025-09-23 21:15:22 +1000

    change order

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,10 @@
 
 set -e
 
+pnpm --filter @skuba-lib/api build
+
 tsx src/skuba build "${@}"
 
 chmod +x 'lib/skuba.js'
 
 pnpm --filter eslint-plugin-skuba build
-
-pnpm --filter @skuba-lib/api build
