ab8db3bbdd4719c4d4610e8fc5d3536a82ecdb34
samchungy <samchungy@gmail.com>
2025-09-26 20:34:17 +1000

    merge

--- src/cli/lint/internalLints/upgrade/patches/12.3.0/index.ts
+++ src/cli/lint/internalLints/upgrade/patches/12.3.0/index.ts
@@ -1,10 +1,15 @@
 import type { Patches } from '../../index.js';
 
 import { tryPatchDockerfileSyntaxDirective } from './patchDockerfileSyntaxDirective.js';
+import { rewriteSrcImports } from './rewriteSrcImports.js';
 
 export const patches: Patches = [
   {
     apply: tryPatchDockerfileSyntaxDirective,
     description: 'Remove Dockerfile syntax directives',
+  },
+  {
+    apply: rewriteSrcImports,
+    description: "Rewrite all 'src' imports to be '#src'",
   },
 ];
