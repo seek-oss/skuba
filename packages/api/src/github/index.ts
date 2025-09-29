b78d4ecf4608760fdf165aaea62e1c720d54c647
Sam Chung <samc@seek.com.au>
2025-09-29 10:55:44 +1000

    Export apiTokenFromEnvironment (#2079)

--- packages/api/src/github/index.ts
+++ packages/api/src/github/index.ts
@@ -1,5 +1,8 @@
 export type { Annotation } from './checkRun.js';
-export { buildNameFromEnvironment } from './environment.js';
+export {
+  apiTokenFromEnvironment,
+  buildNameFromEnvironment,
+} from './environment.js';
 export { createCheckRun } from './checkRun.js';
 export { enabledFromEnvironment } from './environment.js';
 export { getPullRequestNumber } from './pullRequest.js';
