496d60ccf8e5e95ef0a96b52ebea4eff9c2b78e1
samchungy <samchungy@gmail.com>
2025-09-29 11:23:52 +1000

    autofix import usage

--- src/cli/lint/internalLints/upgrade/patches/12.3.0/index.ts
+++ src/cli/lint/internalLints/upgrade/patches/12.3.0/index.ts
@@ -1,10 +1,15 @@
 import type { Patches } from '../../index.js';
 
+import { tryPatchApiTokenFromEnvironment } from './patchApiTokenFromEnvironment.js';
 import { tryPatchDockerfileSyntaxDirective } from './patchDockerfileSyntaxDirective.js';
 
 export const patches: Patches = [
   {
     apply: tryPatchDockerfileSyntaxDirective,
     description: 'Remove Dockerfile syntax directives',
+  },
+  {
+    apply: tryPatchApiTokenFromEnvironment,
+    description: 'Update import for `apiTokenFromEnvironment`',
   },
 ];
