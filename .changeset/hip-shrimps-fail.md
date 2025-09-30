9a5e13c14aeb89c4090b9ec672ab505e725a30d2
Sam Chung <samc@seek.com.au>
2025-09-30 16:50:58 +1000

    Commit Crimes (#2086)
    Co-authored-by: Ryan Ling <ryan@outlook.com.au>

---
+++ .changeset/hip-shrimps-fail.md
@@ -0,0 +1,5 @@
+---
+'@skuba-lib/api': patch
+---
+
+types: Fix `Node16` module resolution compatibility
---
+++ packages/api/scripts/fix-types.sh
@@ -0,0 +1,3 @@
+# Temporary workaround for https://github.com/sxzz/rolldown-plugin-dts/issues/95#issuecomment-3350067001
+find lib -name "*.d.ts" -o -name "*.d.mts" | xargs sed -i.bak 's/import { Octokit } from "@octokit\/rest" with { "resolution-mode": "import" };/import type { Octokit } from "@octokit\/rest" with { "resolution-mode": "import" };/g'
+find lib -name "*.bak" -delete
