d916c265c646a69677017ae1479c91263fc1dfe6
Zac Brydon <52645024+zbrydon@users.noreply.github.com>
2025-12-17 20:44:14 +1100

    `tsdown` eslint-config-skuba

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,8 @@ pnpm --filter @skuba-lib/api build
 
 pnpm --filter @skuba-lib/vitest-koa-mocks build
 
+pnpm --filter eslint-config-skuba build
+
 pnpm --filter eslint-plugin-skuba build
 
 pnpm --filter skuba-dive build
