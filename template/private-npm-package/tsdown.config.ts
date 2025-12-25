0fd40315543742fe04f926144a76197d7fc1246f
Zac Brydon <52645024+zbrydon@users.noreply.github.com>
2025-12-25 21:20:26 +1100

    Add types and swap to `lib` output

--- template/private-npm-package/tsdown.config.ts
+++ template/private-npm-package/tsdown.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   exports: true,
+  outDir: 'lib',
   dts: {
     // Bundles type declarations for the specified packages
     resolve: ['sury'],
