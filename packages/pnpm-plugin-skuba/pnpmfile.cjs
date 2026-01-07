29130adc213b70f846d1fe8e1323ce297ed9b083
Zac Brydon <52645024+zbrydon@users.noreply.github.com>
2026-01-08 08:46:02 +1100

    Add `strictDepBuilds` and `trustPolicy`

--- packages/pnpm-plugin-skuba/pnpmfile.cjs
+++ packages/pnpm-plugin-skuba/pnpmfile.cjs
@@ -22,7 +22,23 @@ const PUBLIC_HOIST_PATTERN = [
   'typescript',
 ];
 
-const ONLY_BUILT_DEPENDENCIES = ['@ast-grep/lang-json'];
+const ONLY_BUILT_DEPENDENCIES = [
+  '@ast-grep/lang-json',
+  '@datadog/native-appsec',
+  '@datadog/native-iast-taint-tracking',
+  '@datadog/native-metrics',
+  '@datadog/pprof',
+  'dd-trace',
+  'esbuild',
+  'protobufjs',
+  'unix-dgram',
+  'unrs-resolver',
+];
+
+const TRUST_POLICY_EXCLUDE = [
+  'pino@9.14.0', // https://github.com/seek-oss/skuba/pull/2180#discussion_r2618371672
+  'semver@5.7.2 || 6.3.1',
+];
 
 module.exports = {
   hooks: {
@@ -31,18 +47,23 @@ module.exports = {
       if (typeof config.publicHoistPattern === 'string') {
         config.publicHoistPattern = [config.publicHoistPattern];
       }
-      config.publicHoistPattern ??= [];
-      config.publicHoistPattern.push(...PUBLIC_HOIST_PATTERN);
-
       config.minimumReleaseAgeExclude ??= [];
       config.minimumReleaseAgeExclude.push(...MINIMUM_RELEASE_AGE_EXCLUDE);
 
       config.onlyBuiltDependencies ??= [];
       config.onlyBuiltDependencies.push(...ONLY_BUILT_DEPENDENCIES);
 
-      config.packageManagerStrictVersion ??= true;
-      config.minimumReleaseAge ??= 4320;
+      config.publicHoistPattern ??= [];
+      config.publicHoistPattern.push(...PUBLIC_HOIST_PATTERN);
+
+      config.trustPolicyExclude ??= [];
+      config.trustPolicyExclude.push(...TRUST_POLICY_EXCLUDE);
+
       config.ignorePatchFailures ??= false;
+      config.minimumReleaseAge ??= 4320;
+      config.packageManagerStrictVersion ??= true;
+      config.strictDepBuilds ??= true;
+      config.trustPolicy ??= 'no-downgrade';
 
       return config;
     },
