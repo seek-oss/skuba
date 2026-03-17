aff7177df8a5e0aa963626d2af34536d157275b1
Zac Brydon <52645024+zbrydon@users.noreply.github.com>
2026-03-17 13:20:18 +1100

    Bump rolldown version and document cli args

--- src/cli/build/rolldown.ts
+++ src/cli/build/rolldown.ts
@@ -1,5 +1,7 @@
 import { exec } from '../../utils/exec.js';
 
 export const rolldown = async (args = process.argv.slice(2)) => {
-  await exec('rolldown', ...['-c', ...args]);
+  // Support additional arguments but enforce the use of a config file.
+  // https://rolldown.rs/guide/getting-started#using-the-config-file
+  await exec('rolldown', ...['--config', ...args]);
 };
