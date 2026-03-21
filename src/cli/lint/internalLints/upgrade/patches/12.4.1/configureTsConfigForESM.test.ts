5104a7d82dc4d149e3f38b05d48871065f692f28
samchungy <samchungy@gmail.com>
2026-03-21 12:22:36 +1100

    fix

--- src/cli/lint/internalLints/upgrade/patches/12.4.1/configureTsConfigForESM.test.ts
+++ src/cli/lint/internalLints/upgrade/patches/12.4.1/configureTsConfigForESM.test.ts
@@ -14,7 +14,7 @@ jest.mock('../../../../../../index.js', () => ({
 
 const volToJson = () => vol.toJSON(process.cwd(), undefined, true);
 
-jest.mock('fs-extra', () => memfs.fs);
+jest.mock('fs-extra', () => memfs);
 jest.mock('fast-glob', () => ({
   __esModule: true,
   default: (pat: string, opts: { ignore: string[] }) =>
