6068a6ae1896e01a6d2fff14ca53e2f89070f6c2
samchungy <samchungy@gmail.com>
2026-03-22 12:05:55 +1100

    Run in parallel

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,8 @@ tsx --conditions @seek/skuba/source src/skuba build "${@}"
 
 chmod +x 'lib/skuba.js'
 
-pnpm --filter @skuba-lib/api \
+pnpm --parallel \
+  --filter @skuba-lib/api \
   --filter eslint-config-skuba \
   --filter eslint-plugin-skuba \
   build
