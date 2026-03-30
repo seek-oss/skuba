7db80c45a5e13a108569d260dda76191026984bf
samchungy <samchungy@gmail.com>
2026-03-31 00:04:42 +1100

    lol

--- packages/detect-invalid-spies/vitest.config.ts
+++ packages/detect-invalid-spies/vitest.config.ts
@@ -2,14 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    coverage: {
-      thresholds: {
-        branches: 100,
-        functions: 100,
-        lines: 100,
-        statements: 100,
-      },
-    },
     exclude: ['**/node_modules'],
   },
 });
