e272be8cface6329522247e41009f51c41d7ba96
samchungy <samchungy@gmail.com>
2026-04-21 10:52:33 +0800

    fix running vitest

--- src/cli/test/index.ts
+++ src/cli/test/index.ts
@@ -1,7 +1,10 @@
-import { exec } from '../../utils/exec.js';
+import { createExec } from '../../utils/exec.js';
 
 export const test = async () => {
   const argv = process.argv.slice(2);
+  const customExec = createExec({
+    localDir: process.cwd(),
+  });
 
-  return exec('vitest', ...argv);
+  return customExec('vitest', ...argv);
 };
