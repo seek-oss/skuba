66346963063e2ad93b9ea5e7f25e6feb10487910
samchungy <samchungy@gmail.com>
2026-05-13 19:45:00 +1000

    nope hacky hack way

--- src/cli/test/index.ts
+++ src/cli/test/index.ts
@@ -1,7 +1,11 @@
-import { exec } from '../../utils/exec.js';
+import { createExec } from '../../utils/exec.js';
 
 export const test = async () => {
   const argv = process.argv.slice(2);
+  const customExec = createExec({
+    // For some reason this impacts the ability for Vitest to find snapshot state
+    preferLocal: false,
+  });
 
-  return exec('vitest', ...argv);
+  return customExec('vitest', ...argv);
 };
