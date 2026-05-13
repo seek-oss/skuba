9bfe69785dfe91bffd76a97bc29d0e7e9b52a687
samchungy <samchungy@gmail.com>
2026-05-13 11:02:06 +1000

    does this work

--- scripts/test-template.sh
+++ scripts/test-template.sh
@@ -95,6 +95,9 @@ cd "${directory}" || exit 1
 echo "--- pnpm add --save-dev ${skuba_tar}"
 pnpm add --save-dev ${skuba_tar}
 
+echo "--- pnpm dedupe"
+pnpm dedupe
+
 echo "--- skuba version ${template}"
 pnpm exec skuba version
 pnpm exec skuba -v
