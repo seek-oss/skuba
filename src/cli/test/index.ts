68baeb805dbaeb4491dedd9f077e0a33879f2d82
samchungy <samchungy@gmail.com>
2026-05-13 16:10:54 +1000

    apparently this should work

--- src/cli/test/index.ts
+++ src/cli/test/index.ts
@@ -3,7 +3,7 @@ import { createExec } from '../../utils/exec.js';
 export const test = async () => {
   const argv = process.argv.slice(2);
   const customExec = createExec({
-    localDir: process.cwd(),
+    cwd: process.cwd(),
   });
 
   return customExec('vitest', ...argv);
