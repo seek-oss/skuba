c99cadc08e9e3abab4fbb38490107620766ce229
Zac Brydon <52645024+zbrydon@users.noreply.github.com>
2026-05-15 23:14:06 +1000

    Remove old configure logic

--- src/cli/configure/modules/index.ts
+++ src/cli/configure/modules/index.ts
@@ -2,23 +2,17 @@ import type { Module, Options } from '../types.js';
 
 import { eslintModule } from './eslint.js';
 import { ignoreModule } from './ignore.js';
-import { nodemonModule } from './nodemon.js';
 import { packageModule } from './package.js';
 import { prettierModule } from './prettier.js';
 import { renovateModule } from './renovate.js';
-import { serverlessModule } from './serverless.js';
-import { tslintModule } from './tslint.js';
 
 export const loadModules = (opts: Options): Promise<Module[]> =>
   Promise.all(
     [
       eslintModule,
       ignoreModule,
-      nodemonModule,
       packageModule,
       prettierModule,
       renovateModule,
-      serverlessModule,
-      tslintModule,
     ].map(async (createModule) => createModule(opts)),
   );
