5e7f99c20ce4b6f2f91eebff5b18fd6cc1e9016f
Sam Chung <samc@seek.com.au>
2026-06-30 10:36:56 +1000

    Restore checkrun (#2501)
    Co-authored-by: skuba <34733141+seek-oss-ci@users.noreply.github.com>

--- src/cli/test/index.ts
+++ src/cli/test/index.ts
@@ -1,4 +1,10 @@
+import { inspect } from 'util';
+
 import { createExec } from '../../utils/exec.js';
+import { log } from '../../utils/logging.js';
+import { throwOnTimeout } from '../../utils/wait.js';
+
+import { createAnnotations } from './annotate.js';
 
 export const test = async () => {
   const argv = process.argv.slice(2);
@@ -7,5 +13,17 @@ export const test = async () => {
     preferLocal: false,
   });
 
-  return customExec('vitest', ...argv);
+  let result: { code: number } | undefined;
+  try {
+    result = await customExec('vitest', ...argv);
+  } catch {
+    process.exitCode = 1;
+  }
+
+  try {
+    await throwOnTimeout(createAnnotations(result?.code === 0), { s: 30 });
+  } catch (err) {
+    log.warn('Failed to annotate test results.');
+    log.subtle(inspect(err));
+  }
 };
