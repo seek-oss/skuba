4df7013b5fa1a595c0591af3ca6bca16e81dbc6c
Sam Chung <samchungy@gmail.com>
2026-08-28 13:58:19 +1000

    this should work

--- config/prettier.js
+++ config/prettier.js
@@ -1,14 +1,8 @@
-/**
- * @see https://prettier.io/docs/configuration
- * @satisfies {import('prettier').Config}
- */
 const config = {
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'all',
-  plugins: [
-    new URL(import.meta.resolve('prettier-plugin-packagejson')).pathname,
-  ],
+  plugins: ['prettier-plugin-packagejson'],
 };
 
 export default config;
