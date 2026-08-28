ee99617b0fc6fcad2e0013d46d9ed3b1156725a5
Sam Chung <samchungy@gmail.com>
2026-08-28 12:45:16 +1000

    update symantics

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,6 +1,6 @@
-import { default as defaultConfig } from 'oxc-config-seek/oxfmt';
+import oxfmtConfig from 'oxc-config-seek/oxfmt';
 import { defineConfig } from 'oxfmt';
 
 export default defineConfig({
-  ...defaultConfig,
+  ...oxfmtConfig,
 });
